892b50d31255a84c7c41de905e73ca7591256f5a
Ikaledaa <123587965+IKaledaa@users.noreply.github.com>
2023-02-17 15:23:23 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// let number = 0;
+// while (number < 6){
+//     console.log(number);
+//     number++;
+// }
+// let num = 0;
+// do{
+//     console.log(num);
+//     num++;
+// } while (num < 6);
+
+// for (let num = 0; num < 5; num++){
+//     console.log(num);
+// }
+
+// let num = 8;
+// while (num){
+//     console.log(num);
+//     num--;
+// }
+
+// for (let num = 0; num < 3; num++){
+//     console.log(`Число: ${num}`);
+// }
+
+// let num=0;
+// while (num < 3){
+//     console.log(`Число: ${num}`);
+//     num++;    
+// }
+
+ first : for (let num = 0; num < 2; num++){
+    for (let size = 0; size < 3; size++){
+        console.log(size);
+        if (size == 1) break;
+    }
+}
